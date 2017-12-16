84c7d0520510776f7ca7fb64c7e38ad6b4ec9dab
Julia Nara <julianara888@gmail.com>
2017-12-16 19:48:56 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,21 @@
+class PigLatinizer
+
+  def piglatinize(word)
+    vowels = ['a','e','i','o','u']
+    consonants = ("a".."z").to_a.delete_if{|l| vowels.include?(l)}
+    if vowels.include?(word[0].downcase)
+      word << "way"
+    else
+      word = word.split('')
+      while consonants.include?(word[0].downcase)
+        l = word.shift
+        word << l
+      end
+      word.join('') << 'ay'
+    end
+  end
+
+  def to_pig_latin(sentence)
+    sentence.split(' ').map { |e| self.piglatinize(e) }.join(' ')
+  end
+end
