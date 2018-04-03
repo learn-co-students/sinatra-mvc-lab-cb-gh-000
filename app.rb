b7abd9657295f9a7dfc1e3be3f52f303bb5ed379
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-03 15:49:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    str = params[:user_phrase]
+    pg = PigLatinizer.new
+    @result = pg.piglatinize(str)
+    erb :piglatinize
+  end
 end
